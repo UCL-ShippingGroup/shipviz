2d96fb1871b037b3265271e348923531c2b95509
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-06-24 16:59:41 +0100

    Run commands as postgres user.

--- cartoDB/utils/installation/postgis_extension.sh
+++ cartoDB/utils/installation/postgis_extension.sh
@@ -1,4 +1,4 @@
-sudo su - postgres
+su - postgres <<'EOF'
 POSTGIS_SQL_PATH=`pg_config --sharedir`/contrib/postgis-2.1
 createdb -E UTF8 template_postgis
 createlang -d template_postgis plpgsql
@@ -8,5 +8,6 @@ psql -d template_postgis -c "CREATE EXTENSION postgis_topology"
 psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
 psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
 exit
+EOF
 
 
