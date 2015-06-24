c3997a26a9a719acca22d5a20782f208edcab872
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-06-24 16:16:54 +0100

    Some installations assume UTF-8, so set that up at the beginning of the script.

--- cartoDB/utils/installation/requirements.sh
+++ cartoDB/utils/installation/requirements.sh
@@ -1,12 +1,12 @@
+#UTF-8
+echo -e 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' | sudo tee /etc/default/locale
+source /etc/default/locale
+
 #GIT
 sudo apt-get -y install git
 
 #VIM
 sudo apt-get -y install vim
-
-#UTF-8
-echo -e 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' | sudo tee /etc/default/locale
-source /etc/default/locale
 
 #MAKE
 sudo apt-get install -y build-essential checkinstall 
