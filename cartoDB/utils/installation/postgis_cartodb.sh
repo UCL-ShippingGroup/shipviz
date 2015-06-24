ae0dec9c1083076f727f01a9b2a5a6794dd35d0c
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-06-24 17:46:21 +0100

    Rework modification of postgresql.conf from script

--- cartoDB/utils/installation/postgis_cartodb.sh
+++ cartoDB/utils/installation/postgis_cartodb.sh
@@ -3,7 +3,7 @@ git clone https://github.com/CartoDB/cartodb-postgresql.git
 cd cartodb-postgresql
 git checkout cdb
 sudo make all install
-sudo echo "shared_preload_libraries = 'schema_triggers.so'" >> /etc/postgresql/9.3/main/postgresql.conf
+echo "shared_preload_libraries = 'schema_triggers.so'" | sudo tee --append /etc/postgresql/9.3/main/postgresql.conf
 sudo service postgresql restart # restart postgres
 sudo PGUSER=postgres make installcheck # to run tests
 
