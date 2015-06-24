ec73191743215bdbb6337d374f60f950de4b5b67
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-06-24 18:40:12 +0100

    Update script names and reorder.

--- cartoDB/utils/installation/cartoDB_install.sh
+++ cartoDB/utils/installation/cartoDB_install.sh
@@ -1,7 +1,8 @@
-./ppas.sh
-./requirements.sh
-./postgres.sh
-./postgis.sh
-./postgis_extension.sh
-./postgis_triggers.sh
-./postgis_cartodb.sh
+./ppas_install.sh
+./requirements_install.sh
+./postgres_install.sh
+./postgis_install.sh
+./ruby_install.sh
+./python_install.sh	
+./nvm_install.sh
+./raster_support.sh
