595ba3cf72c6366a2e6d61831cf15bf7f17d2ac9
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-06-25 14:07:06 +0100

    Refactored requirements script.

--- cartoDB/utils/installation/requirements_install.sh
+++ cartoDB/utils/installation/requirements_install.sh
@@ -2,57 +2,12 @@
 echo -e 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' | sudo tee /etc/default/locale
 source /etc/default/locale
 
-#GIT
-sudo apt-get -y install git
-
-#VIM
-sudo apt-get -y install vim
-
-#CURL
-sudo apt-get install -y curl
-
-#MAKE
-sudo apt-get install -y build-essential checkinstall 
-
-#UNP
-sudo apt-get install -y unp
-
-#ZIP
-sudo apt-get install -y zip
-
-#GEOS
-sudo apt-get install -y libgeos-c1 libgeos-dev
-
-#GDAL
-sudo apt-get install -y gdal-bin libgdal1-dev
-
-#JSON-C
-sudo apt-get install -y libjson0 python-simplejson libjson0-dev
-
-#PROJ4
-sudo apt-get install -y proj-bin proj-data libproj-dev
-
-#NODEJS
-sudo apt-get install -y nodejs-legacy
-sudo apt-get install -y npm
-sudo apt-get install -y libpango1.0-dev
-
-#REDIS SERVER
-sudo apt-get install -y redis-server
-
-#PYTHON 
-sudo apt-get install -y python2.7-dev
-sudo apt-get install -y python-setuptools
-
-#MapNik
-sudo apt-get install -y libmapnik-dev python-mapnik mapnik-utils
-
-#ImageMagick
-sudo apt-get install -y imagemagick
+for req in git vim curl build-essential checkinstall unp zip libgeos-c1 libgeos-dev gdal-bin libgdal1-dev libjson0 python-simplejson libjson0-dev proj-bin proj-data libproj-dev nodejs-legacy npm  libpango1.0-dev redis-server python2.7-dev python-setuptools libmapnik-dev python-mapnik mapnik-utils imagemagick xsltproc dblatex; do
+	printf "\n\n"
+	sudo apt-get install -y $req
+done
 
 #Varnish - no access to versions  < 3.x needed by CartoDB
 #sudo apt-get install -y varnish=2.1.5.1-cdb1 #or any version <3.x
 
-#Tools for autogenerating postgis docu
-sudo apt-get install -y xsltproc  dblatex
 
