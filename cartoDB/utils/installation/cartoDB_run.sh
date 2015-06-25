bd64146960401f566d15eeb23a244397f0e05b4a
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-06-25 12:03:17 +0100

    Add full paths to cartoDB executables.

--- cartoDB/utils/installation/cartoDB_run.sh
+++ cartoDB/utils/installation/cartoDB_run.sh
@@ -23,16 +23,16 @@ bundle install
 cp $HOME/cartodb/config/app_config.yml.sample $HOME/cartodb/config/app_config.yml
 
 #Configure database details
-cp $HOME/config/database.yml.sample $HOME/config/database.yml
+cp $HOME/cartodb/config/database.yml.sample $HOME/cartodb/config/database.yml
 
 #Add entries to /etc/hosts needed in development
 echo "127.0.0.1 ${SUBDOMAIN}.localhost.lan" | sudo tee -a /etc/hosts
 
 #Create a development user
-sh script/create_dev_user ${SUBDOMAIN}
+sh  $HOME/cartodb/script/create_dev_user ${SUBDOMAIN}
 
 #Start the resque daemon (needed for import jobs):
-bundle exec script/resque &
+bundle exec  $HOME/cartodb/script/resque &
 
 #Finally, start the CartoDB development server on port 3000:
 bundle exec rails s -p 3000 &
