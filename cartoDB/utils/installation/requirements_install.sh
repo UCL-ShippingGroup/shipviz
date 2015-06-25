903e7d9408250144280789c98c9cfa4702aa5889
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-06-25 14:59:50 +0100

    Removed dblatex from list: takes too long to install and it's just nice-to-have.

--- cartoDB/utils/installation/requirements_install.sh
+++ cartoDB/utils/installation/requirements_install.sh
@@ -2,7 +2,7 @@
 echo -e 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' | sudo tee /etc/default/locale
 source /etc/default/locale
 
-for req in git vim curl build-essential checkinstall unp zip libgeos-c1 libgeos-dev gdal-bin libgdal1-dev libjson0 python-simplejson libjson0-dev proj-bin proj-data libproj-dev nodejs-legacy npm  libpango1.0-dev redis-server python2.7-dev python-setuptools libmapnik-dev python-mapnik mapnik-utils imagemagick xsltproc dblatex; do
+for req in git vim curl build-essential checkinstall unp zip libgeos-c1 libgeos-dev gdal-bin libgdal1-dev libjson0 python-simplejson libjson0-dev proj-bin proj-data libproj-dev nodejs-legacy npm  libpango1.0-dev redis-server python2.7-dev python-setuptools libmapnik-dev python-mapnik mapnik-utils imagemagick xsltproc; do
 	printf "\n\n"
 	sudo apt-get install -y $req
 done
