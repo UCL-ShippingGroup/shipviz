a041958739893bc1e3a5af015cdfddde8eca9c9a
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-07-14 18:07:56 +0100

    Create SPH files for each of the areas in the US ECA csv file.

--- ECA/csv2shp.py
+++ ECA/csv2shp.py
@@ -8,8 +8,8 @@ filename = "dec_US-ECA-lonlat.csv"
 ecas = pandas.read_csv(filename)
 
 #For each ECA area, create a polygon in the shapefile
-w = shapefile.Writer(shapefile.POLYGON)
 for group in ecas.groupby('Area'):
+    w = shapefile.Writer(shapefile.POLYGON)
     area = group[0]
     data = group[1]
     latitudes = data['Latitude']
@@ -17,12 +17,9 @@ for group in ecas.groupby('Area'):
     w.poly(parts = [numpy.column_stack((longitudes, latitudes)).tolist()])
     w.field('FIRST_FLD','C','150')
     w.record(FIRST_FLD = area)
-
-#Save shapefile along with dbf and spx
-w.save(filename)
-
-#Shapefiles need a prj file for CartoDB to import properly
-prj = open("%s.prj" % filename, "w")
-epsg = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]'
-prj.write(epsg)
-prj.close()
+    w.save(area)
+    #Shapefiles need a prj file for CartoDB to import properly
+    prj = open("%s.prj" % area, "w")
+    epsg = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]'
+    prj.write(epsg)
+    prj.close()
